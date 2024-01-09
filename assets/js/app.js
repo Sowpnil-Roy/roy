f3d09d7e992619959f28268da12e501fe89f0c0d
Sowpnil Roy <86656724+Sowpnil-Roy@users.noreply.github.com>
2024-01-10 01:46:15 +0600

    About Me added

--- assets/js/app.js
+++ assets/js/app.js
@@ -110,7 +110,12 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" >Develop a passion for learning. If you do, you will never cease to grow.</p>
+            <p class="tag" >Currently studying in <b>Ahsanullah University of Science and Technology</b> Department of Computer Science and 
+      Enginnering <b><i>(CSE)</i></b>. I'm a Tech Enthusiast person who works with web development, app development, UI/UX Design most 
+      of the time. Other than that I do Graphics Designing 
+      or Photography by passion. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design 
+      problem at a time. Hope you get what you are looking for Keep pray for me.
+      Thank You</p>
     </div>
 
     
